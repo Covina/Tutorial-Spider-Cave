429876cad37af9c8725c6247f0cbfd2c6408987d
Covina <jabs303@gmail.com>
2017-04-07 08:54:06 -0700

    Section 6, Lecture 98;  Created Door animations, acquiring Collectables;  Door opens when all collected.

---
+++ Assets/Scripts/Collectables/Collectables.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectables : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		// add to the total collectables count;
+		Door.instance.collectablesCount++;
+
+	}
+
+
+	// Collect the collectables
+	void OnTriggerEnter2D (Collider2D collider)
+	{
+		// if its the player, decrement the total
+		if (collider.gameObject.tag == "Player") {
+
+			if (Door.instance != null) {
+				Door.instance.DecrementCollectables ();
+			}
+
+			// "Collect" the gem by destroying it.
+			Destroy(gameObject);
+
+		}
+
+	}
+
+
+}
---
+++ Assets/Scripts/Objects/Door.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+	public static Door instance;
+
+	private Animator animator;
+	private BoxCollider2D box;
+
+	// accessible from elsewhere, just not needed in inspector
+	[HideInInspector] public int collectablesCount;
+
+
+
+	void Awake ()
+	{
+		// create instance of the door
+		MakeInstance();
+
+		// Animator
+		animator = GetComponent<Animator>();
+
+		// Door Box Collider
+		box = GetComponent<BoxCollider2D>();
+
+	}
+
+
+//	// Use this for initialization
+//	void Start () {
+//		
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+
+	// Create singleton
+	void MakeInstance ()
+	{
+		if (instance == null) {
+			instance = this;
+		}
+
+	}
+
+	// Door opener
+	IEnumerator OpenDoor ()
+	{
+		animator.Play("door-open");
+
+		yield return new WaitForSeconds (0.7f);
+
+		// enable that the box collider is a trigger now
+		box.isTrigger = true;
+		
+	}
+
+	// If the door is open and the player walks through it, WIN!
+	void OnTriggerEnter2D (Collider2D collider)
+	{
+
+		if (collider.gameObject.tag == "Player") {
+
+			// win level
+			Debug.Log("Level Completed!");
+		}
+
+	}
+
+	// Reduce count of collectables when collected
+	public void DecrementCollectables ()
+	{
+		collectablesCount--;
+
+		// if we've acquired them all, then open the door!
+		if (collectablesCount == 0) {
+
+			StartCoroutine( OpenDoor() );
+
+		}
+
+	}
+
+
+
+
+} // Door
