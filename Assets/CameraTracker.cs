4e269d7130d18955f1c5840b4fa52ea585b3e082
Covina <jabs303@gmail.com>
2017-04-07 23:51:02 -0700

    Section 6, Lecture 101;  Building the levels (partial)

---
+++ Assets/CameraTracker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTracker : MonoBehaviour {
+
+
+	public GameObject player;
+
+	private float offsetY = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+
+		Camera.main.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + offsetY, -10);
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		Camera.main.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + offsetY, -10);
+
+	}
+}
